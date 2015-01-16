f637ad26af22a96625bafe889864445596b9fd91
Tony Lattke <tonylattke@gmail.com>
2015-01-16 16:27:02 +0100

    Data Structures Vector and Matrix

---
+++ 2_data_structures.m
@@ -0,0 +1,91 @@
+####################################### Vector ##########################################
+
+# Vector 2
+myvector2 = [ "Bruce" "Wayne" ]
+
+# Vector 3
+myvector3 = [ "Red", "Green", "Blue" ]
+
+# Vector N
+myvectorn = [ "Batman" "Superman" "Wonder Woman" "Green Light" "Flash" "J\'onn"]
+
+# Access
+printf("%s\n",myvectorn(1:6));
+
+####################################### Matrix ##########################################
+
+# Empty Matrix 4x4 with 0 values
+printf("Matrix zeros\n");
+mymatrix = zeros(4,4)
+# Diagonal with ones 		-- eye(rows,colums)
+# All elements with ones 	-- ones(rows,colums)
+# All elements random values-- rand(rows,colums)
+
+# Create matrix 2x3 with initials values
+printf("Matrix loaded\n");
+mymatrix = [ 4 8 15 ; 16 23 42]
+
+printf("Matrix Transpose and conjugate\n");
+# Transpose and conjugate matrix
+mymatrix'
+
+printf("Matrix Transpose\n");
+# Transpose matrix
+mymatrix.'
+
+# Operate matrix
+printf("Operate Matrix\n");
+aux = ones(2,3);
+mymatrix + aux		# Plus
+mymatrix - aux		# Minus
+mymatrix * aux'		# Multiply
+
+# A*A
+printf("Multiply Matrix A*A\n");
+squares = [ 1 2 3; 4 5 6; 7 8 9 ];
+squares^2
+
+# Access
+printf("Access\n");
+squares(5)   # Access to the fifth element on matrix
+squares(3,3) # Access to the 3,3 element on matrix
+squares(1:3) # Access to the 3 elements on first colum
+squares(2,:) # Access second row
+squares(:,3) # Access third columm
+
+# Length
+printf("Length of row %d\n", length(squares));
+[rows,colums] = size(squares)
+
+# Diagonal functionas
+diag(squares) 	# Get Diagonal on matrix
+diag([1 2 3])	# Create matrix with diagonal 1 2 3
+
+# Solve linear system Ax = b
+A = [4 8 15; 16 23 42; 65 31 12];
+b = [1 2 3]';
+A \ b
+
+# Inverse
+inv(A)
+
+# LU-decomposition
+[L,U,P] = lu(A)
+
+# QR-decomposition
+[Q,R] = qr(A)
+
+# Cholesky decomposition
+#chol(A)
+
+# Singular values
+svd(A)
+
+# Hessenberg form
+hess(A)
+
+# Eigenvalues
+eig(A)
+
+# The condition number of A with respect to the 2-norm.
+cond(A)
--- README.md
+++ README.md
@@ -11,3 +11,4 @@ octave file_name
 | --------------------- |------------------------------------------------------------- |
 | 0_hello_world.m 		| - Hello world to Octave |
 | 1_variables.m 		| - Using variables |
+| 2_data_structures.m	| - Using basic data structures (Vector, Matrix) |
